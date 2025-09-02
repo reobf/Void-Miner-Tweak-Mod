fe3fefbc935c56d9efc71e3595cf91d0caf4ce07
PinkYuDeer <wer00001@outlook.com>
2025-09-02 23:55:32 +0800

    old code work best

--- src/main/java/reobf/vmtweak/main/VMTweak.java
+++ src/main/java/reobf/vmtweak/main/VMTweak.java
@@ -1,10 +1,10 @@
 package reobf.vmtweak.main;
 
-import java.lang.reflect.Field;
 import java.util.*;
 
-import net.minecraft.world.WorldProvider;
-import net.minecraft.world.chunk.IChunkProvider;
+import galacticgreg.api.ModDimensionDef;
+import galacticgreg.registry.GalacticGregRegistry;
+import gtneioreplugin.util.DimensionHelper;
 import net.minecraft.world.gen.ChunkProviderServer;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.MinecraftForge;
@@ -21,8 +21,6 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import gtneioreplugin.util.DimensionHelper;
-import micdoodle8.mods.galacticraft.api.galaxies.CelestialBody;
 
 @Mod(
     modid = "vmtweak",
@@ -34,10 +32,6 @@ public class VMTweak {
 
     public static Logger LOGGER;
 
-    public static List<String> dimName = Arrays.asList(DimensionHelper.DimName);
-    public static List<String> dimNameTrimmed = Arrays.asList(DimensionHelper.DimNameTrimmed);
-    public static List<String> dimNameShort = Arrays.asList(DimensionHelper.DimNameDisplayed);
-    public static HashMap<Integer, String> dimMap = new HashMap<>();
     public static BiMap<Integer, String> dimDirectMap = HashBiMap.create();
     public static HashMap<Integer, String> cache = new HashMap<>();
 
@@ -62,94 +56,48 @@ public class VMTweak {
         dimDirectMap.put(1, "ED");
         dimDirectMap.put(7, "TF");
         // Ross128b 维度
-        dimDirectMap.put(getRoss128bDimID(false), "Rb");
-        dimDirectMap.put(getRoss128bDimID(true), "Ra");
+        dimDirectMap.put(bartworks.common.configs.Configuration.crossModInteractions.ross128BAID, "Rb");
+        dimDirectMap.put(bartworks.common.configs.Configuration.crossModInteractions.ross128BID, "Ra");
     }
 
     @SubscribeEvent
     public void onWorldLoad(WorldEvent.Load e) {
-        if (dimDirectMap.containsKey(e.world.provider.dimensionId)) return;
+        int dimID = e.world.provider.dimensionId;
+        if (dimDirectMap.containsKey(dimID)) return;
         try {
-            // 访问当前已经拥有 DimID 的维度，获取他们对应的 名字、ID和ChunkProvider。
-            Class<?> clazz = DimensionManager.class;
-            Field providersField = clazz.getDeclaredField("providers");
-            providersField.setAccessible(true);
-            @SuppressWarnings("unchecked")
-            Hashtable<Integer, Class<? extends WorldProvider>> providers = (Hashtable<Integer, Class<? extends WorldProvider>>) providersField
-                .get(null);
-            // 遍历所有WorldProvider
-            for (Map.Entry<Integer, Class<? extends WorldProvider>> entry : providers.entrySet()) {
-                int dimID = entry.getKey();
-                Class<? extends WorldProvider> providerClass = entry.getValue();
-                if (dimMap.containsKey(dimID) || dimDirectMap.containsKey(dimID)) continue;
-                switch (providerClass.getName()) {
-                    case "com.rwtema.extrautils.worldgen.Underdark.WorldProviderUnderdark":
-                        dimMap.put(dimID, "Underdark");
-                        cache.put(dimID, "com.rwtema.extrautils.worldgen.Underdark.ChunkProviderUnderdark");
-                        continue;
-                    case "toxiceverglades.world.WorldProviderMod":
-                        dimMap.put(dimID, "ToxicEverglades");
-                        cache.put(dimID, "toxiceverglades.chunk.ChunkProviderModded");
-                        continue;
-                    case "me.eigenraven.personalspace.world.PersonalWorldProvider":
-                        dimMap.put(dimID, "PersonalWorld");
-                        cache.put(dimID, "me.eigenraven.personalspace.world.PersonalChunkProvider");
-                        continue;
-                    case "appeng.spatial.StorageWorldProvider":
-                        dimMap.put(dimID, "StorageWorld");
-                        cache.put(dimID, "appeng.spatial.StorageChunkProvider.StorageChunkProvider");
-                        continue;
-                    case "com.rwtema.extrautils.worldgen.endoftime.WorldProviderEndOfTime":
-                        dimMap.put(dimID, "EndOfTime");
-                        cache.put(dimID, "com.rwtema.extrautils.worldgen.endoftime.ChunkProviderEndOfTime");
-                        continue;
-                    case "thaumcraft.common.lib.world.dim.WorldProviderOuter":
-                        dimMap.put(dimID, "Outer");
-                        cache.put(dimID, "thaumcraft.common.lib.world.dim.ChunkProviderOuter");
-                        continue;
-                    case "makeo.gadomancy.common.utils.world.WorldProviderTCEldrich":
-                        dimMap.put(dimID, "TCEldrich");
-                        cache
-                            .put(dimID, "makeo.gadomancy.common.utils.world.ChunkProviderTCOuter#ChunkProviderTCOuter");
-                        continue;
-                    case "twilightforest.world.WorldProviderTwilightForest":
-                        dimMap.put(dimID, "TwilightForest");
-                        cache
-                            .put(dimID, "twilightforest.world.ChunkProviderTwilightForest");
-                        continue;
-                }
-                // 星系维度自动获取
-                WorldProvider provider = providerClass.newInstance();
-                @SuppressWarnings("unchecked")
-                String chunkProviderName = ((Class<? extends IChunkProvider>) providerClass
-                    .getMethod("getChunkProviderClass")
-                    .invoke(provider)).getName();
-                String dimName = ((CelestialBody) providerClass.getMethod("getCelestialBody")
-                    .invoke(provider)).getName();
-                // 获取chunkProviderClass的name
-                dimMap.put(dimID, dimName);
-                cache.put(dimID, chunkProviderName);
-            }
+            String chunkProviderName = ((ChunkProviderServer) e.world.getChunkProvider())
+                .currentChunkProvider.getClass()
+                .getName();
+            cache.put(dimID,chunkProviderName);
+            if (chunkProviderName.equals("com.rwtema.extrautils.worldgen.Underdark.ChunkProviderUnderdark"))
+                dimDirectMap.put(dimID, "DD");
+            else {
+                List<String> dimName = Arrays.asList(DimensionHelper.DimName);
+                String name = GalacticGregRegistry.getModContainers()
+                    .stream()
+                    .flatMap(
+                        modContainer -> modContainer.getDimensionList()
+                            .stream())
+                    .filter(s -> {
+                            if (DimensionManager.getWorld(dimID) == null) return false;
 
-        } catch (Exception exception) {
-            LOGGER.error("Error loading world providers: ", exception);
-        }
-        for (Map.Entry<Integer, String> dimMap : dimMap.entrySet()) {
-            // 将dimID和dimNameShort加入dimDirectMap
-            int dimID = dimMap.getKey();
-            String dimNameMap = dimMap.getValue();
-            for (int i = 0; i < dimNameTrimmed.size(); i++) {
-                if (dimNameMap.equalsIgnoreCase(dimNameTrimmed.get(i)) || dimNameMap.equalsIgnoreCase(dimName.get(i))) {
-                    dimDirectMap.put(dimID, dimNameShort.get(i));
-                    break;
+                            return s.getChunkProviderName()
+                                .equals(
+                                    DimensionManager.getProvider(dimID)
+                                        .createChunkGenerator()
+                                        .getClass()
+                                        .getName());
+                        }
+                    )
+                    .map(ModDimensionDef::getDimIdentifier)
+                    .findFirst()
+                    .orElse(null);
+                int index;
+                if ((index = dimName.indexOf(name)) >= 0) {
+                    dimDirectMap.forcePut(dimID, DimensionHelper.DimNameDisplayed[index]);
                 }
             }
-        }
-    }
-
-    protected static int getRoss128bDimID(boolean isRoss128ba) {
-        return isRoss128ba ? bartworks.common.configs.Configuration.crossModInteractions.ross128BID
-            : bartworks.common.configs.Configuration.crossModInteractions.ross128BAID;
+        } catch (Exception ignored) {}
     }
 
 }

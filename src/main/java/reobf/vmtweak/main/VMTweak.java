172cec9edbd72cd4d65ca75865a35eb78b23542d
PinkYuDeer <wer00001@outlook.com>
2025-09-02 21:11:27 +0800

    enable DeepDark and EndAsteroids mine

--- src/main/java/reobf/vmtweak/main/VMTweak.java
+++ src/main/java/reobf/vmtweak/main/VMTweak.java
@@ -1,10 +1,13 @@
 package reobf.vmtweak.main;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.*;
 
-import net.minecraft.world.gen.ChunkProviderServer;
+import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import micdoodle8.mods.galacticraft.api.galaxies.CelestialBody;
+import net.minecraft.world.WorldProvider;
+import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.WorldEvent;
@@ -16,9 +19,8 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import galacticgreg.api.ModDimensionDef;
-import galacticgreg.registry.GalacticGregRegistry;
 import gtneioreplugin.util.DimensionHelper;
+import org.apache.logging.log4j.Logger;
 
 @Mod(
     modid = "vmtweak",
@@ -28,87 +30,114 @@ import gtneioreplugin.util.DimensionHelper;
     acceptedMinecraftVersions = "[1.7.10]")
 public class VMTweak {
 
+    public static Logger LOGGER;
+
+    public static List<String> dimName = Arrays.asList(DimensionHelper.DimName);
+    public static List<String> dimNameTrimmed = Arrays.asList(DimensionHelper.DimNameTrimmed);
+    public static List<String> dimNameShort = Arrays.asList(DimensionHelper.DimNameDisplayed);
+    public static HashMap<Integer, String> dimMap = new HashMap<>();
+    public static BiMap<Integer, String> dimDirectMap = HashBiMap.create();
+    public static HashMap<Integer, String> cache = new HashMap<>();
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        LOGGER = event.getModLog(); // 获取模组专用的Logger
+    }
+
+
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-
         MinecraftForge.EVENT_BUS.register(this);
         FMLCommonHandler.instance()
             .bus()
             .register(this);
     }
 
-    public static List<String> dimName = Arrays.asList(DimensionHelper.DimName);
-    // public static List<String> dimNameShort = Arrays.asList(DimensionHelper.DimNameDisplayed);
-
-    public static BiMap<Integer, String> dimMapping = HashBiMap.create();
-    public static HashMap<Integer, String> cache = new HashMap<>();
-
-    @SubscribeEvent
-    public void a(WorldEvent.Load e) {
-        for (int i : DimensionManager.getStaticDimensionIDs()) {
-            if (dimMapping.containsKey(i)) continue;
-            String name = getNameForID(i);
-
-            int index;
-            if ((index = dimName.indexOf(name)) >= 0) {
-                dimMapping.forcePut(i, DimensionHelper.DimNameDisplayed[index]);
-                // dimDefMapping.forcePut(def, DimensionHelper.DimNameDisplayed[index]);
-            }
-        }
-
-        try {
-            cache.put(
-                e.world.provider.dimensionId,
-                ((ChunkProviderServer) e.world.getChunkProvider()).currentChunkProvider.getClass()
-                    .getName());
-        } catch (Exception ignored) {}
-
+    @Mod.EventHandler
+    public void onModLoadingComplete(FMLLoadCompleteEvent event) {
+        // 原版维度、暮色维度
+        dimDirectMap.put(0, "Ow");
+        dimDirectMap.put(-1, "Ne");
+        dimDirectMap.put(1, "ED");
+        dimDirectMap.put(7, "TF");
+        // Ross128b 维度
+        dimDirectMap.put(getRoss128bDimID(false), "Rb");
+        dimDirectMap.put(getRoss128bDimID(true), "Ra");
     }
 
-    public static String getNameForID(int id) {
-        if (id == bartworks.common.configs.Configuration.crossModInteractions.ross128BID) {
-            return "Ross128b";
-        }
-        if (id == bartworks.common.configs.Configuration.crossModInteractions.ross128BAID) {
-            return "Ross128ba";
-        }
-        if (id == 0) {
-            return "Overworld";
-        }
-        if (id == -1) {
-            return "Nether";
-        }
-        if (id == 7) {
-            return "Twilight";
-        }
-        if (id == 1) {
-            return "TheEnd";
-        }
-
-        return GalacticGregRegistry.getModContainers()
-            .stream()
-            .flatMap(
-                modContainer -> modContainer.getDimensionList()
-                    .stream())
-
-            .filter(s -> {
-
-                if (DimensionManager.getWorld(id) == null) return false;
-
-                return s.getChunkProviderName()
-                    .equals(
-                        DimensionManager.getProvider(id)
-                            .createChunkGenerator()
-                            .getClass()
-                            .getName());
-
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load e) {
+        if (dimDirectMap.containsKey(e.world.provider.dimensionId)) return;
+        try {
+            // 访问当前已经拥有 DimID 的维度，获取他们对应的 名字、ID和ChunkProvider。
+            Class<?> clazz = DimensionManager.class;
+            Field providersField = clazz.getDeclaredField("providers");
+            providersField.setAccessible(true);
+            @SuppressWarnings("unchecked") Hashtable<Integer, Class<? extends WorldProvider>> providers =
+                (Hashtable<Integer, Class<? extends WorldProvider>>) providersField.get(null);
+            // 遍历所有WorldProvider
+            for (Map.Entry<Integer, Class<? extends WorldProvider>> entry : providers.entrySet()) {
+                int dimID = entry.getKey();
+                Class<? extends WorldProvider> providerClass = entry.getValue();
+                if (dimMap.containsKey(dimID) || dimDirectMap.containsKey(dimID)) continue;
+                switch (providerClass.getName()) {
+                    case "com.rwtema.extrautils.worldgen.Underdark.WorldProviderUnderdark":
+                        dimMap.put(dimID, "Underdark");
+                        cache.put(dimID, "com.rwtema.extrautils.worldgen.Underdark.ChunkProviderUnderdark");
+                        continue;
+                    case "toxiceverglades.world.WorldProviderMod":
+                        dimMap.put(dimID, "ToxicEverglades");
+                        cache.put(dimID, "toxiceverglades.chunk.ChunkProviderModded");
+                        continue;
+                    case "me.eigenraven.personalspace.world.PersonalWorldProvider":
+                        dimMap.put(dimID, "PersonalWorld");
+                        cache.put(dimID, "me.eigenraven.personalspace.world.PersonalChunkProvider");
+                        continue;
+                    case "appeng.spatial.StorageWorldProvider":
+                        dimMap.put(dimID, "StorageWorld");
+                        cache.put(dimID, "appeng.spatial.StorageChunkProvider.StorageChunkProvider");
+                        continue;
+                    case "com.rwtema.extrautils.worldgen.endoftime.WorldProviderEndOfTime":
+                        dimMap.put(dimID, "EndOfTime");
+                        cache.put(dimID, "com.rwtema.extrautils.worldgen.endoftime.ChunkProviderEndOfTime");
+                        continue;
+                }
+                // 星系维度自动获取
+                WorldProvider provider = providerClass.newInstance();
+                @SuppressWarnings("unchecked") String chunkProviderName =
+                    ((Class<? extends IChunkProvider>)providerClass
+                        .getMethod("getChunkProviderClass")
+                        .invoke(provider))
+                        .getName();
+                String dimName = ((CelestialBody)providerClass
+                        .getMethod("getCelestialBody")
+                        .invoke(provider)).getName();
+                // 获取chunkProviderClass的name
+                dimMap.put(dimID, dimName);
+                cache.put(dimID, chunkProviderName);
             }
 
-            )
-            .map(ModDimensionDef::getDimIdentifier)
-            .findFirst()
-            .orElse(null);
+        } catch (Exception exception) {
+            LOGGER.error("Error loading world providers: ", exception);
+        }
+        for (Map.Entry<Integer, String> dimMap : dimMap.entrySet()) {
+            // 将dimID和dimNameShort加入dimDirectMap
+            int dimID = dimMap.getKey();
+            String dimNameMap = dimMap.getValue();
+            for (int i = 0; i < dimNameTrimmed.size(); i++) {
+                if (dimNameMap.equalsIgnoreCase(dimNameTrimmed.get(i)) ||
+                    dimNameMap.equalsIgnoreCase(dimName.get(i))) {
+                    dimDirectMap.put(dimID, dimNameShort.get(i));
+                    break;
+                }
+            }
+        }
+    }
 
+    protected static int getRoss128bDimID(boolean isRoss128ba) {
+        return isRoss128ba ?
+            bartworks.common.configs.Configuration.crossModInteractions.ross128BID :
+            bartworks.common.configs.Configuration.crossModInteractions.ross128BAID;
     }
 
 }
--- src/main/java/reobf/vmtweak/main/mixin/mixins/MTEVoidMinerBaseMixin.java
+++ src/main/java/reobf/vmtweak/main/mixin/mixins/MTEVoidMinerBaseMixin.java
@@ -3,11 +3,13 @@ package reobf.vmtweak.main.mixin.mixins;
 import java.util.Objects;
 import java.util.Optional;
 
+import bwcrossmod.galacticgreg.VoidMinerUtility;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.gen.ChunkProviderServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -30,77 +32,129 @@ import reobf.vmtweak.main.VMTweak;
 
 @Mixin(MTEVoidMinerBase.class)
 public abstract class MTEVoidMinerBaseMixin<T extends MTEVoidMinerBase<T>> extends MTEEnhancedMultiBlockBase<T> {
-
-    @Shadow(remap = false)
-    private float totalWeight;
-
-    @Unique
-    private int VMTweak$dim;
-
-    @Unique
-    private String VMTweak$mLastDimensionOverride = "None";
-
     public MTEVoidMinerBaseMixin(String aName) {
         super(aName);
     }
 
+    @Shadow(remap = false)
+    private float totalWeight;
+
+    @Shadow(remap = false)
+    private VoidMinerUtility.DropMap dropMap;
+
+    @Unique
+    private String VMTweak$warning = "";
+
+    @Unique
+    private String VMTweak$mLastDimensionOverride = "None";
+
+    // 有些mod会在这个方法里直接用id来判断维度添加产物。
     @ModifyVariable(method = "handleExtraDrops", at = @At("HEAD"), require = 1, remap = false, argsOnly = true)
     private int handleExtraDrops(int id) {
-        return VMTweak.dimMapping.inverse()
-            .getOrDefault(VMTweak$a(), id);
+        return VMTweak.dimDirectMap.inverse()
+            .getOrDefault(VMTweak$checkNEIOreBlockDim(), id);
     }
 
-    @ModifyVariable(method = "handleModDimDef", at = @At("HEAD"), require = 1, remap = false, argsOnly = true)
-    private int handleModDimDef(int id) {
-        return VMTweak$dim = VMTweak.dimMapping.inverse()
-            .getOrDefault(VMTweak$a(), id);
-    }
+    // 这个方法首先会根据id来判断是否有对应的掉落辞典，如果没有的话则会通过chunkProviderName查找它给自己创建的映射
+    @Inject(method = "handleModDimDef", at = @At("HEAD"), require = 1, remap = false, cancellable = true)
+    private void handleModDimDef(int id, CallbackInfo ci) {
+        ci.cancel();
 
-    @ModifyVariable(method = "handleModDimDef", at = @At("STORE"), require = 1, remap = false)
-    private String handleModDimDef0(String id) {
-        return VMTweak.cache.getOrDefault(VMTweak$dim, id);
+        // 判断能否覆写维度id
+        VMTweak$warning = "";
+        if (VMTweak.dimDirectMap.inverse().containsKey(VMTweak$checkNEIOreBlockDim())) {
+            int dimId = VMTweak.dimDirectMap.inverse().get(VMTweak$checkNEIOreBlockDim());
+            if (VoidMinerUtility.dropMapsByDimId.containsKey(dimId)) {
+                this.dropMap = VoidMinerUtility.dropMapsByDimId.get(dimId);
+                return;
+            }
+        } else if (Objects.equals(VMTweak$mLastDimensionOverride, "EA")) {
+            int dimId = 1;
+            if (VoidMinerUtility.dropMapsByDimId.containsKey(dimId)) {
+                this.dropMap = VoidMinerUtility.dropMapsByDimId.get(dimId);
+                return;
+            }
+        } else {
+            if (VMTweak.cache.containsKey(id)) {
+                String chunkProviderName = VMTweak.cache.get(id);
+                if (VoidMinerUtility.dropMapsByChunkProviderName.containsKey(chunkProviderName)) {
+                    VoidMinerUtility.DropMap tempDropMap =
+                        VoidMinerUtility.dropMapsByChunkProviderName.get(chunkProviderName);
+                    if (tempDropMap.getTotalWeight()>0){
+                        this.dropMap = tempDropMap;
+                        return;
+                    }
+                }
+            }
+        }
+
+        // 如果不能覆写维度id，则使用原本的逻辑检测当前维度
+        if (VoidMinerUtility.dropMapsByDimId.containsKey(id)) {
+            this.dropMap = VoidMinerUtility.dropMapsByDimId.get(id);
+            VMTweak$warning = StatCollector.translateToLocal("gui.dimension.overwrite.failed");
+            return;
+        } else {
+            String chunkProviderName = null;
+            if (this.getBaseMetaTileEntity() != null) {
+                chunkProviderName = ((ChunkProviderServer) this.getBaseMetaTileEntity()
+                    .getWorld()
+                    .getChunkProvider()).currentChunkProvider.getClass()
+                    .getName();
+            }
+
+            if (VoidMinerUtility.dropMapsByChunkProviderName.containsKey(chunkProviderName)) {
+                this.dropMap = VoidMinerUtility.dropMapsByChunkProviderName.get(chunkProviderName);
+                VMTweak$warning = StatCollector.translateToLocal("gui.dimension.overwrite.failed");
+                return;
+            }
+        }
+
+        // 如果当前维度也没有对应的掉落辞典，则清空掉落辞典，并提示
+        this.dropMap = new VoidMinerUtility.DropMap();
     }
 
     @Unique
-    private String VMTweak$a() {
-        return Optional.ofNullable(this.mInventory[1])
+    private String VMTweak$checkNEIOreBlockDim() {
+        String neiOreBlockDim = Optional.ofNullable(this.mInventory[1])
             .filter(s -> s.getItem() instanceof ItemDimensionDisplay)
             .map(ItemDimensionDisplay::getDimension)
             .orElse("None");
+        if (!Objects.equals(neiOreBlockDim, VMTweak$mLastDimensionOverride)) {
+            VMTweak$mLastDimensionOverride = neiOreBlockDim;
+            totalWeight = 0;
+        }
+        return neiOreBlockDim;
     }
 
     @Inject(method = "saveNBTData", at = @At("HEAD"), require = 1, remap = false)
     public void saveNBTData(NBTTagCompound aNBT, CallbackInfo c) {
-        aNBT.setString("mLastDimensionOverride", this.VMTweak$mLastDimensionOverride);
+        aNBT.setString("VMTweak$mLastDimensionOverride", this.VMTweak$mLastDimensionOverride);
     }
 
     @Inject(method = "loadNBTData", at = @At("HEAD"), require = 1, remap = false)
     public void loadNBTData(NBTTagCompound aNBT, CallbackInfo c) {
-        this.VMTweak$mLastDimensionOverride = aNBT.getString("mLastDimensionOverride");
+        this.VMTweak$mLastDimensionOverride = aNBT.getString("VMTweak$mLastDimensionOverride");
     }
 
     @Inject(method = "working", at = @At("HEAD"), require = 1, remap = false)
     public void working(CallbackInfoReturnable<Boolean> c) {
-        String dim = Optional.ofNullable(this.mInventory[1])
-            .filter(s -> s.getItem() instanceof ItemDimensionDisplay)
-            .map(ItemDimensionDisplay::getDimension)
-            .orElse("None");
-
-        if (!Objects.equals(dim, VMTweak$mLastDimensionOverride)) {
-            VMTweak$mLastDimensionOverride = dim;
-            totalWeight = 0;
-        }
+        VMTweak$checkNEIOreBlockDim();
     }
 
     @Unique
-    private String VMTweak$get() {
+    private String VMTweak$getGuiText() {
         String ext = null;
         try {
             Block block = ModBlocks.getBlock(VMTweak$mLastDimensionOverride);
             ext = new ItemStack(block).getDisplayName();
         } catch (Exception ignored) {}
         ext = ext == null ? VMTweak$mLastDimensionOverride : ext;
-        ext = Objects.equals(ext, "None") ? "" : StatCollector.translateToLocal("gui.dimension.overwrite.description") + ext;
+        ext = Objects.equals(VMTweak$mLastDimensionOverride, "EA") ?
+            StatCollector.translateToLocal("gui.dimension.overwrite.EndAsteroids") : ext;
+        ext = Objects.equals(ext, "None") ?
+            "" : StatCollector.translateToLocal("gui.dimension.overwrite.description") + ext;
+        if (!ext.isEmpty() && !VMTweak$warning.isEmpty() && this.dropMap!=null && this.dropMap.getTotalWeight()<=0)
+            return VMTweak$warning;
         return ext;
     }
 
@@ -108,7 +162,7 @@ public abstract class MTEVoidMinerBaseMixin<T extends MTEVoidMinerBase<T>> exten
     protected void drawTexts(DynamicPositionedColumn screenElements, SlotWidget inventorySlot) {
         super.drawTexts(screenElements, inventorySlot);
         screenElements.widget(
-            TextWidget.dynamicString(this::VMTweak$get)
+            TextWidget.dynamicString(this::VMTweak$getGuiText)
                 .setSynced(true)
                 .setTextAlignment(Alignment.CenterLeft)
                 .setEnabled(true));
